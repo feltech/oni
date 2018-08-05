0cb28d4311d3abaf500a4464a310a141c24801a9
David Feltell <dave@feltell.net>
2018-08-06 00:01:32 +0100

    Handle menu focus as a special higher priority case

--- browser/src/Input/KeyboardInput.tsx
+++ browser/src/Input/KeyboardInput.tsx
@@ -87,13 +87,13 @@ export class KeyboardInputView extends React.PureComponent<
         if (this.props.onActivate) {
             this._removeExistingDisposables()
             const d1 = this.props.onActivate.subscribe(() => {
-                focusManager.setFocus(this._keyboardElement)
+                focusManager.pushFocus(this._keyboardElement)
             })
             this._disposables.push(d1)
         }
 
         if (this.props.startActive && this._keyboardElement) {
-            focusManager.setFocus(this._keyboardElement)
+            focusManager.pushFocus(this._keyboardElement)
         }
     }
 
--- browser/src/Services/FocusManager.ts
+++ browser/src/Services/FocusManager.ts
@@ -2,6 +2,7 @@
  * FocusManager.ts
  */
 
+import * as _ from "lodash"
 import * as Log from "oni-core-logging"
 
 class FocusManager {
@@ -12,27 +13,36 @@ class FocusManager {
     }
 
     public pushFocus(element: HTMLElement) {
-        this._focusElementStack = [element, ...this._focusElementStack]
+        // Remove any previous entry in the stack.
+        this._popFocus(element)
+        // Check if this element or the currently focussed element is a modal-type menu.
+        const elIsMenu = isMenu(element)
+        const currentIsMenu = isMenu(this.focusedElement)
+
+        if (elIsMenu || !currentIsMenu) {
+            // Either this is a menu element, or it isn't and no menus are open, so put on top
+            // of stack.
+            this._focusElementStack.unshift(element)
+        } else {
+            // A menu is open, so push "underneath" the menu in the stack.
+            const idx = _.findLastIndex(this._focusElementStack, el => isMenu(el))
+            this._focusElementStack.splice(idx + 1, 0, element)
+        }
 
         window.setTimeout(() => this.enforceFocus(), 0)
     }
 
     public popFocus(element: HTMLElement) {
-        this._focusElementStack = this._focusElementStack.filter(elem => elem !== element)
-
+        this._popFocus(element)
         this.enforceFocus()
     }
 
-    public setFocus(element: HTMLElement): void {
-        if (element) {
-            this._focusElementStack = [element]
-            element.focus()
-        } else {
-            Log.warn("FocusManager.setFocus called with null element")
-        }
-    }
-
     public enforceFocus(): void {
+        // Clean the focus stack of elements that no longer exist (e.g. after a re-render
+        // following alt-tabbing away).
+        const removed = _.remove(this._focusElementStack, el => !el.offsetParent)
+        Log.debug(`Removed ${removed.length} destroyed elements from focus stack`)
+
         if (this._focusElementStack.length === 0) {
             return
         }
@@ -42,6 +52,13 @@ class FocusManager {
             activeElement.focus()
         }
     }
+
+    private _popFocus(element: HTMLElement) {
+        this._focusElementStack = this._focusElementStack.filter(elem => elem !== element)
+    }
 }
 
+function isMenu(el: HTMLElement) {
+    return el && el.offsetParent && el.offsetParent.classList.contains("menu")
+}
 export const focusManager = new FocusManager()
